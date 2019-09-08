ee5495087fa021de7c667caefcc5d4666b638017
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-09-08 13:10:32 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :student
+
+  get "/students", to: "students#index", as: "students"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
